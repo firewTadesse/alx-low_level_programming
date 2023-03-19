96d44d23678dcb61c0be399bc51c718478d19262
firewTadesse <firewtadesse1990@gmail.com>
2023-03-19 04:32:43 -0700

    four-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main -a program the prints a line with puts function
+ * Return: 0(success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
