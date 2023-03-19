e6e3026c81ec13ef6af560efeef050fd5d53fbf0
firewTadesse <firewtadesse1990@gmail.com>
2023-03-19 05:33:34 -0700

    six-size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,6 +8,7 @@ int main(void)
 	char a;
 	int b;
 	long int c;
+	long long int d;
 	float f;
 printf("Size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
