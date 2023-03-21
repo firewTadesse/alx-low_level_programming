7e14eb9f08bba981d8a4e32c54ae0ecf3ca83aee
firewTadesse <firewtadesse1990@gmail.com>
2023-03-21 11:44:03 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,9 +12,9 @@ void print_alphabet_x10(void)
 	int j;
 
 	for (i = 1; i <= 10; i++)
-		for (j = 97; j <= 122; j++)
-		{
+	{
+		for (j = 'a'; j <= 'z'; j++)
 			_putchar(j);
-		}
 	_putchar('\n');
+	}
 }
