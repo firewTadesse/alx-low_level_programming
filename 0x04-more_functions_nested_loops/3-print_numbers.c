aac1d10d9945f221151812fd1065c00dc1c64eea
firewTadesse <firewtadesse1990@gmail.com>
2023-03-23 12:16:16 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - print 0-9
- * return: void
+ * return: return the number from 0 to 9
  */
 
 void print_numbers(void)
 {
-	char c;
+	int x;
 
-	for (c = '0'; c <= '9'; c++)
+	for (x = 0; x <= 9; x++)
 	{
-		_putchar(c);
+		_putchar(x + '0')
 	}
 	_putchar('\n');
 }
