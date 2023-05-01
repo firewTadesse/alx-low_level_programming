f875eebe13414db5baa42650f45a98088330f593
firewTadesse <firewtadesse1990@gmail.com>
2023-05-01 01:00:01 -0700

    upadated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,15 +6,15 @@
  *
  * Return: number of nodes
  */
-size_t print_listint(const listint_t *k)
+size_t print_listint(const listint_t *h)
 {
 	size_t num = 0;
 
-	while (k)
+	while (h)
 	{
-		printf("%d\n", k->n);
+		printf("%d\n", h->n);
 		num++;
-		k = k->next;
+		h = h->next;
 	}
 
 	return (num);
